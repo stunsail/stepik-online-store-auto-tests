d3067fdb4f58a0c85b91cb34687f43907cfd5c5c
stunsail <38725897+stunsail@users.noreply.github.com>
2020-01-18 23:32:30 -0500

    fix typos

--- pages/locators.py
+++ pages/locators.py
@@ -10,9 +10,6 @@ class BasketPageLocators():
     BASKET_ITEMS = (By.CSS_SELECTOR, ".basket-items")
     BASKET_EMPRY_MESSAGE = (By.CSS_SELECTOR, "#content_inner p")
 
-#class MainPageLocators():
-#    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
@@ -20,7 +17,6 @@ class LoginPageLocators():
     REGISTRATION_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
     REGISTRATION_CONFIRM_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password2")
     REGISTER_BUTTON = (By.CSS_SELECTOR, "button.btn[name = 'registration_submit']")
-
 
 class ProductPageLocators():
     ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
